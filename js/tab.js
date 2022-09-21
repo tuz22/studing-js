0f06df357c7c6e82747006b740bb16831ef308ef
Seungji <96293047+tuz22@users.noreply.github.com>
2022-09-22 00:16:32 +0900

    update tab키, dataset.id

--- js/tab.js
+++ js/tab.js
@@ -3,16 +3,23 @@ let tabBtns = document.querySelectorAll('.tab-button');
 let tabContents = document.querySelectorAll('.tab-content');
 console.log(tabBtns.length);
 
-for (let i = 0; i < tabBtns.length; i++){
-  tabBtns[i].addEventListener('click', function(){
-    console.log(i+'번눌림')
-    document.querySelector('.orange').classList.remove('orange');
-    tabBtns[i].classList.add('orange');
-  
-    document.querySelector('.show').classList.remove('show');
-    tabContents[i].classList.add('show');
-  });
+// for (let i = 0; i < tabBtns.length; i++){
+//   tabBtns[i].addEventListener('click', function(){
+//     openTab(i);
+//   });
+// }
+
+document.querySelector('.list').addEventListener('click', function(e){
+  // if (e.target == document.querySelectorAll('.tab-button')[0]){
+  //   openTab(0)
+  // }
+  openTab(e.target.dataset.id);
+});
+
+function openTab(numb) {
+  document.querySelector('.orange').classList.remove('orange');
+  tabBtns[numb].classList.add('orange');
+
+  document.querySelector('.show').classList.remove('show');
+  tabContents[numb].classList.add('show');
 }
-
-
-
