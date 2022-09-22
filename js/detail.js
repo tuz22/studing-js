a31efebd5b1728c99fed9e9f3790d3ceb0405b20
tuz22 <seungji.dev@gmail.com>
2022-09-22 19:47:55 +0900

    array 자료형, object 자료형

--- js/detail.js
+++ js/detail.js
@@ -23,3 +23,20 @@ function openTab(numb) {
   document.querySelector('.show').classList.remove('show');
   tabContents[numb].classList.add('show');
 }
+
+/* array 자료형: 순서대로 저장 */
+let fruit = ['사과', 3000, 'red'];
+fruit[0] = '체리'; // 자료 수정
+fruit[3] = '할인'; // 자료 추가
+console.log(fruit)
+console.log(fruit[0])
+
+fruit.sort(); // 자료 정렬
+fruit.slice(1,2); // 자료 자르기
+console.log(fruit.slice(0,2));
+
+/* object 자료형: key(이름)로 저장 */
+let fruit2 = {name : '사과', price : 3000}; // key : value 형태
+fruit2.name = '체리'; // 자료 수정
+console.log(fruit2)
+console.log(fruit2.name)
